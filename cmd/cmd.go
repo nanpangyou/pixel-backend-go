3a6e7677af9780df86cf665fb7690f81ec1f7bd2
nanpangyou <yigehd@foxmail.com>
2023-11-15 23:28:42 +0800

    test run server r.run() 的下一行不会执行

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -1,10 +1,18 @@
 package cmd
 
 import (
+	"log"
+
 	"github.com/nanpangyou/pixel-backend-go/internal/router"
 )
 
 func RunServer() {
+	log.Println("Server started================")
 	r := router.New()
-	r.Run(":8080")
+	log.Println("============Server started")
+	err := r.Run(":8080")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Panicln("Run 的下一行")
 }
