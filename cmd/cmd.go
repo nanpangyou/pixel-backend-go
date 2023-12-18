1ad0e02a6255132cd535f53441baa46c3e951af3
nanpangyou <yigehd@foxmail.com>
2023-12-18 23:30:15 +0800

    import cobra to build cli

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -5,15 +5,49 @@ import (
 
 	"github.com/nanpangyou/pixel-backend-go/internal/database"
 	"github.com/nanpangyou/pixel-backend-go/internal/router"
+	"github.com/spf13/cobra"
 )
 
-func RunServer() {
+func Run() {
+
+	rootCmd := &cobra.Command{
+		Use: "pixel-backend-go",
+	}
+
+	dbCmd := &cobra.Command{
+		Use: "db",
+		// Run: func(cmd *cobra.Command, args []string) {
+		// database.MySQLConnect()
+		// database.MySQLCreatTable()
+		// defer database.MySQLClose()
+		// },
+	}
+
+	createCmd := &cobra.Command{
+		Use: "create",
+		Run: func(cmd *cobra.Command, args []string) {
+			database.PgCreateTable()
+		},
+	}
+
+	srvCmd := &cobra.Command{
+		Use: "server",
+		Run: func(cmd *cobra.Command, args []string) {
+			RunServer()
+		},
+	}
+
+	rootCmd.AddCommand(dbCmd)
+	rootCmd.AddCommand(srvCmd)
+	dbCmd.AddCommand(createCmd)
+
 	database.PgConnect()
-	database.PgCreateTable()
 	defer database.PgClose()
-	database.MySQLConnect()
-	database.MySQLCreatTable()
-	defer database.MySQLClose()
+	rootCmd.Execute()
+
+}
+
+func RunServer() {
 	r := router.New()
 	err := r.Run(":8080")
 	if err != nil {
